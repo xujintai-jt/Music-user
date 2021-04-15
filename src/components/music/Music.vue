67ceb6deb631cf512bfffa39c960fe5d586f4a2b
xujintai-jt <1791639435@qq.com>
2021-04-15 11:57:26 +0800

    完成music页面(播放次数排序)

--- src/components/music/Music.vue
+++ src/components/music/Music.vue
@@ -2,25 +2,231 @@
  * @Author: xujintai
  * @Date: 2021-04-08 17:07:55
  * @LastEditors: xujintai
- * @LastEditTime: 2021-04-14 21:19:21
+ * @LastEditTime: 2021-04-15 11:55:14
  * @Description: file content
- * @FilePath: \music-shop\src\components\music\Music.vue
+ * @FilePath: \music-user\src\components\music\Music.vue
 -->
 <template>
   <div id="music">
-    <div>
-      <div>
-        <span>黄种人--谢霆锋</span>
-        <el-button type="primary" icon="el-icon-star-on">收藏</el-button>
-        <el-button type="primary" icon="el-icon-star-on">已收藏</el-button>
+    <!-- 搜索音乐 -->
+    <input type="text" v-model="searchName" placeholder="搜索音乐" />
+    <button @click="searchSong()">搜索</button>
+    <!-- 歌曲数据表 -->
+    <div style="width:100%;background-color:#f40;">
+      <el-table :data="allSongs" class="song-table" style="width: 100%" border>
+        <el-table-column type="index" label="序号" align="center"></el-table-column>
+        <el-table-column label="音乐名" prop="songName" align="center"></el-table-column>
+        <el-table-column prop="artist" label="歌手" align="center"></el-table-column>
+        <el-table-column align="center" width="120" label="音乐封面">
+          <template slot-scope="scope">
+            <img
+              :src="'http://localhost:8633/api/music/poster?img=' + scope.row.poster"
+              style="width:35px;height:35px;"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column align="center" prop="language" label="音乐语种"></el-table-column>
+        <el-table-column align="center" prop="style" label="音乐风格"></el-table-column>
+        <el-table-column align="center" prop="playcount" label="音乐播放次数"></el-table-column>
+        <el-table-column align="center" prop="date" label="音乐最近更新时间"></el-table-column>
+        <el-table-column label="播放" align="center">
+          <template slot-scope="scope">
+            <el-button @click="playMusic(scope.row)">{{scope.row.songName}}</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="收藏" align="center">
+          <template slot-scope="scope">
+            <el-button icon="el-icon-star-off" @click="collectionMusic(scope.row)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <!-- 分页 -->
+
+    <!-- <Col span="24" style="text-align:right;padding:10px;">
+      <el-pagination
+        v-if="paginations.total > 0"
+        :page-sizes="paginations.page_sizes"
+        :page-size="paginations.page_size"
+        :layout="paginations.layout"
+        :total="paginations.total"
+        :current-page.sync="paginations.page_index"
+        @current-change="handleCurrentChange"
+        @size-change="handleSizeChange"
+      ></el-pagination>
+    </Col>-->
+
+    <!-- 播放器 -->
+    <div class="music-audio" v-if="audioIsShow">
+      <div class="music-audio-right">
+        <img :src="activePoster" class="audioPoster" />
+        <!-- 歌手信息 -->
+        <div>
+          <i class="el-icon-service"></i>
+          <span style="margin-left:8px">{{artist}} - {{songName}}</span>
+        </div>
+        <i class="el-icon-loading"></i>
       </div>
+      <audio
+        autoplay="autoplay"
+        id="audio"
+        controls="controls"
+        loop="loop"
+        preload="auto"
+        :src="activeMusicSrc"
+      ></audio>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isfirst: false,
+      firstEdit: true,
+      isCheckPassword: false,
+      inputPassword: "",
+      searchName: "",
+      flag: "", // 用来判断是 添加, 编辑或删除
+      noChangeSongs: [],
+      toListen: false,
+      toListenSrc: "",
+      toListenPoster: "",
+      allSongs: [],
+      allTableData: [],
+      paginations: {
+        // 分页属性
+        page_index: 1, // 当前位于哪页
+        total: 0, // 总数
+        page_size: 8, // 1页显示多少条
+        page_sizes: [8, 15, 20, 25], //每页显示多少条
+        layout: "total, sizes, prev, pager, next, jumper", // 翻页属性
+      },
+      //当前播放的音乐id
+      activeMusicId: "",
+      activePosterSrc: "",
+      //音乐播放器是否显示
+      audioIsShow: false,
+    };
+  },
+  created() {
+    this.getAllSong();
+  },
+  methods: {
+    // 获取所有歌曲
+    getAllSong() {
+      this.$axios
+        .get("http://localhost:8633/api/music/all")
+        .then((res) => {
+          if (res.data.length) {
+            this.allSongs = res.data;
+            this.noChangeSongs = res.data;
+            this.allTableData = res.data;
+            this.setPaginations();
+          } else {
+            console.error(res);
+          }
+        })
+        .catch((err) => console.log(err));
+    },
+    // 设置当前页
+    handleCurrentChange(page) {
+      // 获取当前页
+      let sortnum = this.paginations.page_size * (page - 1);
+      let table = this.allTableData.filter((item, index) => {
+        return index >= sortnum;
+      });
+      // 设置默认分页数据
+      this.allSongs = table.filter((item, index) => {
+        return index < this.paginations.page_size;
+      });
+      this.allSongs = table.filter((item, index) => {
+        return index < this.paginations.page_size;
+      });
+    },
+    // 改变每页条数
+    handleSizeChange(page_size) {
+      // 切换size
+      this.paginations.page_index = 1;
+      this.paginations.page_size = page_size;
+      this.allSongs = this.allTableData.filter((item, index) => {
+        return index < page_size;
+      });
+    },
+    // 设置的分页
+    setPaginations() {
+      // 总页数
+      this.paginations.total = this.allTableData.length;
+      this.paginations.page_index = 1;
+      this.paginations.page_size = 8;
+      // 设置默认分页数据
+      this.allSongs = this.allTableData.filter((item, index) => {
+        return index < this.paginations.page_size;
+      });
+    },
+    //播放音乐
+    playMusic(row) {
+      //点击播放显示播放器
+      this.audioIsShow = true;
+      this.activeMusicId = row._id;
+      this.activePosterSrc = row.poster;
+      this.songName = row.songName;
+      this.artist = row.artist;
+      //刷新页面获取最新播放次数信息
+      this.getAllSong();
+    },
+    //收藏音乐
+    collectionMusic(row) {
+      console.log(row);
+    },
+    //搜索音乐
+    searchSong() {
+      console.log(this.searchName.trim());
+      if (this.searchName.trim().length) {
+        this.$axios
+          .post("http://localhost:8633/api/music/search/byname", {
+            searchName: this.searchName,
+          })
+          .then((res) => {
+            this.allSongs = res.data;
+            this.allTableData = res.data;
+            this.setPaginations();
+          });
+      } else {
+        this.$message.warning("搜索内容不能为空..");
+      }
+    },
+  },
+  computed: {
+    activeMusicSrc() {
+      return `http://localhost:8633/api/music/nowmusic?id=${this.activeMusicId}`;
+    },
+    activePoster() {
+      return `http://localhost:8633/api/music/poster?img=${this.activePosterSrc}`;
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped lang="scss">
+.music-audio {
+  display: flex;
+  width: 60vw;
+  margin: 0;
+  margin: auto;
+  margin-top: 100px;
+  justify-content: space-around;
+  color: #c20c0c;
+  .music-audio-right {
+    width: 20vw;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    .audioPoster {
+      width: 35px;
+      height: 35px;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import "@/assets/css/index.scss"
+import Axios from "axios"
+Vue.prototype.$axios= Axios
 
 Vue.use(ElementUI);
 
